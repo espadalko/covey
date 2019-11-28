5ab6b0fe71f202c6cb1310f23385e1ff3be9c893
espadalko <espadalko@me.com>
2019-11-28 16:23:07 +0700

    parseTextIntoParagraphs

--- src/components/Test1.vue
+++ src/components/Test1.vue
@@ -1,6 +1,13 @@
 <template>
 	<div>
-		Test1
+		<div>
+			<input type="file" @change="saveOriginFileContents($event)">
+			<button @click="parseTextIntoParagraphs()">Параграфы</button>
+			<button @click="save()">Save</button>
+		</div>
+		<div>
+			{{covey.origin}}
+		</div>
 	</div>
 </template>
 
@@ -8,12 +15,38 @@
 	export default {
 		data(){
 			return{
-
+				covey: {},
 			}
+		},
+		mounted(){
+			let localCovey = localStorage.getItem('localCovey')
+			this.covey = JSON.parse(localCovey) || {}
+		},
+		methods:{
+			saveOriginFileContents(event){
+				let reader = new FileReader();
+				reader.onload =(event)=> {
+					this.covey.origin = event.target.result;
+				};
+				reader.onerror = function(event) {
+					console.error("Файл не может быть прочитан! код " + event.target.error.code);
+				};
+				reader.readAsText(event.target.files[0]);
+			},
+			parseTextIntoParagraphs(){
+				// let re = /[А-ЯA-Z]((т.п.|т.д.|пр.)|[^?!.\(]|\([^\)]*\))*[.?!]/g
+				// let sentences = this.original.match(re)
+				let re = /\n/g
+				this.covey.paragraphs = this.covey.original.split(re)
+			},
+			save(){
+				localStorage.setItem('localCovey', JSON.stringify(this.covey))
+			},
 		}
 	}
 </script>
 
 <style lang="stylus">
-	
+	button
+		margin-right 5px	
 </style>
